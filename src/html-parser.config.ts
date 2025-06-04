6cb6ba192dca4497da0e986a9169b0662ab51f4d
アニップ <Hanivan@users.noreply.github.com>
2025-06-04 17:11:03 +0700

    refactor: simplify logger level configuration in HtmlParserConfig
    - Updated the `HtmlParserConfig` interface to use `LogLevel` from `@nestjs/common`, allowing for a more standardized logging level configuration.
    - Removed explicit string literals for logger levels in favor of the imported `LogLevel` type, enhancing type safety and clarity.
    
    Signed-off-by: Hanivan Rizky S <hanivan20@gmail.com>

--- src/html-parser.config.ts
+++ src/html-parser.config.ts
@@ -1,11 +1,7 @@
+import { LogLevel } from '@nestjs/common';
+
 export const HTML_PARSER_LOGGER_LEVEL = 'HTML_PARSER_LOGGER_LEVEL';
 
 export interface HtmlParserConfig {
-  loggerLevel?:
-    | 'log'
-    | 'error'
-    | 'warn'
-    | 'debug'
-    | 'verbose'
-    | Array<'log' | 'error' | 'warn' | 'debug' | 'verbose'>;
+  loggerLevel?: LogLevel | Array<LogLevel>;
 }
